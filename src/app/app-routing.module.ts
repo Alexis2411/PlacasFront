0273c5a1e30a127269e68c59526ba549065c9ffa
Alexis2411 <zs19004866@estudiantes.uv.mx>
2022-06-14 22:53:58 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { DashboardComponent } from './vistas/dashboard/dashboard.component';
+const routes: Routes = [
+  { path:'' , redirectTo:'dashboard' , pathMatch:'full'},
+  {path: 'dashboard', component:DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+export const routingComponents = [DashboardComponent]
